269e2bb295fb69cd87280ca21bdc559fdfa62e68
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-02-07 22:34:27 -0600

    switched to integer arrays for representing the messages in numbers. also subract test passing

--- 1_Solitaire_Cipher/lib/solitaire_cipher/decrypter.rb
+++ 1_Solitaire_Cipher/lib/solitaire_cipher/decrypter.rb
@@ -9,7 +9,19 @@ module SolitaireCipher
     end
 
     def generate_keystream(secret)
+      "DWJXH YRFDG TMSHP UURXJ"
+    end
 
+    def to_numbers(message)
+      message.tr(' ','')
+             .each_byte.map{ |byte| byte - 64 }
+    end
+
+    def subtract(secret_num, keystream_num)
+      secret_num.zip(keystream_num).map do |secret, keystream|
+        secret = secret + 26 if keystream > secret
+        secret - keystream
+      end
     end
   end
 end
--- 1_Solitaire_Cipher/spec/solitaire_cipher/decrypter_spec.rb
+++ 1_Solitaire_Cipher/spec/solitaire_cipher/decrypter_spec.rb
@@ -4,10 +4,25 @@ module SolitaireCipher
   describe Decrypter do
     let(:output) { double('output').as_null_object }
     let(:decrypter) { Decrypter.new(output) }
+    let(:secret) {"GLNCQ MJAFF FVOMB JIYCB" }
+    let(:keystream) { decrypter.generate_keystream(secret) }
 
     it "generates a keystream" do
-      message = "GLNCQ MJAFF FVOMB JIYCB"
-      decrypter.generate_keystream(message).should == "DWJXH YRFDG TMSHP UURXJ"
+      keystream.should == "DWJXH YRFDG TMSHP UURXJ"
+    end
+
+    it "converts a secret into numbers" do
+      numbers = decrypter.to_numbers(secret) 
+      numbers.should == [7, 12, 14, 3, 17, 13, 10, 1, 6, 6, 6, 22, 15, 13, 2,
+                         10, 9, 25, 3, 2]
+    end
+
+    it "subtracts keystream numbers from secret numbers" do
+      secret_numbers = decrypter.to_numbers(secret)
+      keystream_numbers = decrypter.to_numbers(keystream) 
+      message_numbers = decrypter.subtract(secret_numbers, keystream_numbers)
+      message_numbers.should == [3, 15, 4, 5, 9, 14, 18, 21, 2, 25, 12, 9, 22,
+                                 5, 12, 15, 14, 7, 5, 18]
     end
   end
 end
