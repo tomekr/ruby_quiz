604fe60e396a8ab7f7c31b810fc3a20e8bd068df
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-02-12 19:54:52 -0600

    generate_keystream working

--- 1_Solitaire_Cipher/lib/solitaire_cipher/deck.rb
+++ 1_Solitaire_Cipher/lib/solitaire_cipher/deck.rb
@@ -8,18 +8,26 @@ module SolitaireCipher
       @deck
     end
 
-    def shift_down(card, spaces)
-      spaces.times do
-        index = @deck.index(card)
-        if index == 53
-          @deck.rotate!(-1)
-          index = 0
-        end
-        temp = @deck[index]
-        @deck[index] = @deck[index+1]
-        @deck[index+1] = temp
-      end
+
+    def move_A
+      move_down( @deck.index('A') )
       @deck
+    end
+
+    def move_B
+      2.times{ move_down( @deck.index('B') ) }
+      @deck
+    end
+
+    # Moves the index one place down while treating the used array
+    # as circular list.
+    def move_down(index)
+      if index == @deck.length - 1
+        @deck[1..1] = @deck[index], @deck[1]
+        @deck.pop
+      else
+        @deck[index], @deck[index + 1] = @deck[index + 1], @deck[index]
+      end
     end
 
     def triple_cut
@@ -27,11 +35,49 @@ module SolitaireCipher
       b_index = @deck.index("B")
 
       if b_index < a_index
-        top_cut = @deck.slice!(0,b_index)
-        bottom_cut = @deck.slice!(a_index, @deck.length)
+        top_cut = @deck[0..(b_index-1)]
+        middle = @deck[b_index..a_index]
+        bottom_cut = @deck[(a_index+1)..(@deck.length-1)]
+      else
+        top_cut = @deck.slice(0, a_index)
+        middle = @deck[a_index..b_index]
+        bottom_cut = @deck[(b_index+1)..(@deck.length-1)]
       end
 
-      @deck = bottom_cut + @deck + top_cut
+      @deck = bottom_cut + middle + top_cut
+    end
+
+    def count_cut
+      count = @deck.pop
+      top_cut = @deck[0..count-1]
+      @deck = @deck[count..@deck.length] + top_cut + Array(count)
+    end
+
+    def output_letter
+      a = @deck.first
+      a = 53 if a.instance_of? String
+      output = @deck[a]
+      if output.instance_of? String
+        nil
+      else
+        output -= 26 if output > 26
+        (output + 64).chr
+      end
+    end
+
+    def generate_keystream(length)
+      keystream = []
+      while length > 0
+        move_A
+        move_B
+        triple_cut
+        count_cut
+        if (out = output_letter)
+          keystream << out
+          length -= 1
+        end
+      end
+      keystream.join
     end
   end
 end
--- 1_Solitaire_Cipher/spec/solitaire_cipher/deck_spec.rb
+++ 1_Solitaire_Cipher/spec/solitaire_cipher/deck_spec.rb
@@ -12,21 +12,14 @@ module SolitaireCipher
     end
 
     it "moves A joker down one card" do
-      deck.shift_down("A", 1).should == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+      deck.move_A.should == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
         13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
         31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
         49, 50, 51, 52, "B", "A"]
     end
 
-    it "moves B joker down one card" do
-      deck.shift_down("B", 1).should == [1, "B", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-        13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-        31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-        49, 50, 51, 52, "A"]
-    end
-
     it "moves B joker down two cards" do
-      deck.shift_down("B", 2).should == [1, 2, "B", 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+      deck.move_B.should == [1, 2, "B", 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
         13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
         31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
         49, 50, 51, 52, "A"]
@@ -34,9 +27,30 @@ module SolitaireCipher
 
     it "triple cuts around the 2 jokers" do
       d = deck
-      d.shift_down("A",1)
-      d.shift_down("B",2)
+      d.move_A
+      d.move_B
       d.triple_cut.should == ["B"] + Array(2..52) + ["A", 1]
+    end
+
+    it "count_cuts the deck" do
+      d = deck
+      d.move_A
+      d.move_B
+      d.triple_cut
+      d.count_cut.should == Array(2..52) + ["A", "B", 1]
+    end
+
+    it "finds the first output letter of an unkeyed deck" do
+      d = deck
+      d.move_A
+      d.move_B
+      d.triple_cut
+      d.count_cut
+      d.output_letter.should == "D"
+    end
+
+    it "finds the first ten output letters of an unkeyed deck" do
+      deck.generate_keystream(10).should == "DWJXHYRFDG"
     end
   end
 end
